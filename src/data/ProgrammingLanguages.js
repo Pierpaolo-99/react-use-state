0b3bd8ffb224f05fb449481e715723de25cd0cbf
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-03-25 15:32:57 +0100

    add titles for programming languages in data file

--- src/data/ProgrammingLanguages.js
+++ src/data/ProgrammingLanguages.js
@@ -2,31 +2,37 @@ const programmingLanguages = [
     {
         id: 1,
         name: "HTML",
+        title: "HyperText Markup Language",
         description: "Un linguaggio di markup per strutturare il contenuto delle pagine web.",
     },
     {
         id: 2,
         name: "CSS",
+        title: "Cascading Style Sheets",
         description: "Un linguaggio per definire lo stile e il layout delle pagine web.",
     },
     {
         id: 3,
         name: "JavaScript",
+        title: "JS",
         description: "Un linguaggio di programmazione per aggiungere interattività alle pagine web.",
     },
     {
         id: 4,
         name: "Node.js",
+        title: "Node",
         description: "Un runtime JavaScript per eseguire codice lato server.",
     },
     {
         id: 5,
         name: "Express",
+        title: "Express.js",
         description: "Un framework minimalista per creare applicazioni web con Node.js.",
     },
     {
         id: 6,
         name: "ReactJS",
+        title: "React",
         description: "Una libreria JavaScript per costruire interfacce utente basate su componenti.",
     },
 ];
