2464bb59ef7249a8ce136347c7c51865cd611fcd
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-03-25 14:48:09 +0100

    initialize React app with Vite, add basic components and styles

---
+++ src/data/ProgrammingLanguage.js
@@ -0,0 +1,28 @@
+const programmingLanguages = [
+    {
+        name: "HTML",
+        description: "Un linguaggio di markup per strutturare il contenuto delle pagine web.",
+    },
+    {
+        name: "CSS",
+        description: "Un linguaggio per definire lo stile e il layout delle pagine web.",
+    },
+    {
+        name: "JavaScript",
+        description: "Un linguaggio di programmazione per aggiungere interattività alle pagine web.",
+    },
+    {
+        name: "Node.js",
+        description: "Un runtime JavaScript per eseguire codice lato server.",
+    },
+    {
+        name: "Express",
+        description: "Un framework minimalista per creare applicazioni web con Node.js.",
+    },
+    {
+        name: "ReactJS",
+        description: "Una libreria JavaScript per costruire interfacce utente basate su componenti.",
+    },
+];
+
+export default programmingLanguages;
